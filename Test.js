de8c4843224388d86280f3f777fe0a763e4699ae
sheikasifahmed <sheikhasifahmed100@gmail.com>
2022-09-27 17:33:13 +0600

    tast completed

--- Test.js
+++ Test.js
@@ -1,19 +1,34 @@
-const longurl = document.getElementById("inp1").value;
-const shorturl = document.getElementById("inp2").value;
+const longurl = document.getElementById("inp1");
+const shorturl = document.getElementById("inp2");
 const shortOutput = document.getElementById("short");
-const longOutput = document.getAnimations("long");
+const longOutput = document.getElementById("long");
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 
-btn1.addEventListener("click", store);
+btn1.addEventListener("click", function () {
+  const url = longurl.value;
 
-function store() {
-  localStorage.setItem("long_url", "longurl");
+  fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.result.short_link);
+
+      const shortUrl = data.result.short_link;
+      shortOutput.textContent = shortUrl;
+
+      store(shortUrl);
+    })
+    .catch((error) => console.log);
+});
+
+function store(shortUrl) {
+  localStorage.setItem(shortUrl, longurl.value);
 }
 
 btn2.addEventListener("click", show);
 
 function show() {
-  const storedValue = localStorage.getItem("long_url");
-  longOutput.textContent = storedValue;
+  const shortUrlInputed = shorturl.value;
+  const longUrlRestored = localStorage.getItem(shortUrlInputed);
+  longOutput.textContent = longUrlRestored;
 }
